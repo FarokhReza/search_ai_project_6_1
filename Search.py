7bb58ddabc20d77c6325a61f86236280c28402f2
farokh <reza.farokh9830@gmail.com>
2023-03-15 16:39:31 +0330

    dfs commit

---
+++ Search.py
@@ -0,0 +1,38 @@
+from Solution import Solution
+from Problem import Problem
+from datetime import datetime
+
+
+class Search:
+    @staticmethod
+
+    def bfs(prb: Problem) -> Solution:  # this method get a first state of Problem and do bfs for find solution if no
+        # solution is find return None else return the solution
+        start_time = datetime.now()
+        queue = []
+        state = prb.initState
+        queue.append(state)
+        while len(queue) > 0:
+            state = queue.pop(0)
+            neighbors = prb.successor(state)
+            for c in neighbors:
+                if prb.is_goal(c):
+                    return Solution(c, prb, start_time)
+                queue.append(c)
+        return None
+
+    @staticmethod
+
+    def dfs(prb: Problem) -> Solution:
+        start_time = datetime.now()
+        stack = []
+        state = prb.initState
+        stack.append(state)
+        while len(stack) > 0:
+            state = stack.pop()
+            if prb.is_goal(state):
+                return Solution(state, prb, start_time)
+            neighbors = prb.successor(state)
+            stack.extend(neighbors)
+        return None
+    
---
+++ State.py
@@ -0,0 +1,22 @@
+# this class only for the first time setup init state for problem and is given to every search
+# به عنوان تابع هش اصلاح شود تا در لوپ نیافتد
+
+class State:
+    def __init__(self, pipes: list, parent, g_n: int, prev_action: tuple):
+        self.pipes = pipes
+        self.parent = parent
+        self.g_n = g_n
+        self.prev_action = prev_action
+
+    def change_between_two_pipe(self, pipe_src_ind: int, pipe_dest_ind: int):
+        self.pipes[pipe_dest_ind].add_ball(self.pipes[pipe_src_ind].remove_ball())
+
+    def __hash__(self):
+        hash_strings = []
+        for i in self.pipes:
+            hash_strings.append(i.__hash__())
+        hash_strings = sorted(hash_strings)
+        hash_string = ''
+        for i in hash_strings:
+            hash_string += i + '###'
+        return hash_string
